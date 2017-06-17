1a02afe7c7ef92092d8e682960ea6dfd75d6cb0d
Pavan Andhukuri <pavan496@gmail.com>
2017-06-18 01:19:30 +0530

    Add session management

--- server/app.js
+++ server/app.js
@@ -1,16 +1,28 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser')
+var session = require('express-session');
 
+//Enabling session management
+var sessionOptions = {
+    secret: 'my-secret',
+    cookie: {}
+};
+app.use(session(sessionOptions));
+
+//Enabling static file serving
 app.use(express.static('static'));
-app.use(bodyParser.json());
-
 app.use('/lib', express.static('bower_components'));
 
+//Enabling HTTP Body parsing
+app.use(bodyParser.json());
+
+//Setting up paths
 console.log('Setting up paths...');
 require('./paths')(app);
 console.log('Done...');
 
+//Starting the server
 var server = app.listen(3000, () => {
     console.log('Server started... Listening on port 3000...')
 });

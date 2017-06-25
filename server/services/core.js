64cbea28f0df7192f28b07918e2720a5a1c983ae
Pavan Andhukuri <pavan496@gmail.com>
2017-06-25 17:29:09 +0530

    Add queueing mechanism to push only one file at a time

--- server/services/core.js
+++ server/services/core.js
@@ -11,24 +11,38 @@ exports.exportPhotos = (oauth2Client, folderId, albumId, userEmail, callback) =>
     var socketService = serviceHolder.getService('socket');
 
     driveService.getFilesFromFolder(oauth2Client, folderId, (err, files) => {
+        if (files.length == 0) {
+            socketService.update(userEmail, 'no_files');
+            socketService.disconnect(userEmail);
+        }
 
         //Send the list of files to the user
         socketService.update(userEmail, 'list_of_files', files);
 
-        //For each file found
-        files.forEach((value, index) => {
-
-            //Fetch the image from Google Drive
-            driveService.getFile(oauth2Client, value.id, (content) => {
-
-                //Push the image to Photos
-                photosService.postFileToPhotos(oauth2Client, content, value.name, value.mimeType, albumId, (message) => {
-                    socketService.update(userEmail, 'completion', value.id);
-                });
-            });
-        });
+        //Push files one after another
+        pushFile(files, oauth2Client, 0, albumId, socketService, userEmail);
     });
 
-    callback('Process Initiated');
+    callback('INITIATED');
+
+
+}
+
+function pushFile(files, oauth2Client, index, albumId, socketService, userEmail) {
+    var file = files[index];
+    driveService.getFile(oauth2Client, file.id, (content) => {
+
+        //Push the image to Photos
+        photosService.postFileToPhotos(oauth2Client, content, file.name, file.mimeType, albumId, (message) => {
+            socketService.update(userEmail, 'completion', file.id);
+
+            if (index + 1 < files.length) {
+                pushFile(files, oauth2Client, index + 1, albumId, socketService, userEmail)
+            } else {
+                socketService.update(userEmail, 'all_files_done');
+                socketService.disconnect(userEmail);
+            }
+        });
+    });
 }
 

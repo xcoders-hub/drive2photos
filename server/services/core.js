168dd6d88e342253727a40ec66e2524a787bfc59
Pavan Andhukuri <pavan496@gmail.com>
2017-06-30 17:03:06 +0530

    Add functionality to maintain transaction status in session

--- server/services/core.js
+++ server/services/core.js
@@ -1,46 +1,60 @@
 var google = require('googleapis');
 var driveService = require('./drive');
 var photosService = require('./photos');
-var serviceHolder = require('./service.holder')
 
 /**
  * Service and controller for handling core functions
  */
-exports.exportPhotos = (oauth2Client, folderId, albumId, userEmail, callback) => {
+exports.exportPhotos = (session, folderId, albumId, callback) => {
 
-    var socketService = serviceHolder.getService('socket');
-
+    var oauth2Client = session.oauth2Client;
     driveService.getFilesFromFolder(oauth2Client, folderId, (err, files) => {
-        if (files.length == 0) {
-            socketService.update(userEmail, 'no_files');
-            socketService.disconnect(userEmail);
+        if (files.length != 0) {
+            session.transactionStatus = {};
+            session.transactionStatus.status = 'started';
+            session.transactionStatus.files = files;
+            session.save();
+
+            //Push files one after another
+            pushFile(files, session, oauth2Client, 0, albumId);
+        } else {
+            session.transactionStatus = {};
+            session.transactionStatus.status = 'completed';
+            session.transactionStatus.files = [];
+            session.save();
         }
-
-        //Send the list of files to the user
-        socketService.update(userEmail, 'list_of_files', files);
-
-        //Push files one after another
-        pushFile(files, oauth2Client, 0, albumId, socketService, userEmail);
     });
 
     callback('INITIATED');
-
-
 }
 
-function pushFile(files, oauth2Client, index, albumId, socketService, userEmail) {
+/**
+ * Function called recursively to queue file pushing one after the other.
+ * 
+ * @param {*} files 
+ * @param {*} session 
+ * @param {*} oauth2Client 
+ * @param {*} index 
+ * @param {*} albumId 
+ * @param {*} userEmail 
+ */
+function pushFile(files, session, oauth2Client, index, albumId) {
     var file = files[index];
     driveService.getFile(oauth2Client, file.id, (content) => {
 
         //Push the image to Photos
         photosService.postFileToPhotos(oauth2Client, content, file.name, file.mimeType, albumId, (message) => {
-            socketService.update(userEmail, 'completion', file.id);
 
+            //Mark the file as completed
+            session.transactionStatus.files[index].isCompleted = true;
+            session.save();
+
+            // Check if more files are pending for pushing and act accordingly.
             if (index + 1 < files.length) {
-                pushFile(files, oauth2Client, index + 1, albumId, socketService, userEmail)
+                pushFile(files, session, oauth2Client, index + 1, albumId)
             } else {
-                socketService.update(userEmail, 'all_files_done');
-                socketService.disconnect(userEmail);
+                session.transactionStatus.status = 'completed';
+                session.save();
             }
         });
     });
